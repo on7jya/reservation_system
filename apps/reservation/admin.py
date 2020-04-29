4b6d0aaf1d69630c4502d3510c311f42170f842c
Ivan.Kataev <Ivan.Kataev@x5.ru>
2020-04-29 21:59:23 +0300

    Заменил в админке регистрацию User на Person, добавил заголовок на русском

--- apps/reservation/admin.py
+++ apps/reservation/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-from .models import Office
+from .models import Office, Person
 
 User = get_user_model()
 
 
-@admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-    list_display = ["username", "email", "last_login", "is_active", "is_superuser"]
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "last_login", "is_active", "is_superuser", "office"]
     search_fields = ["username"]
 
 
--- apps/reservation/apps.py
+++ apps/reservation/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ReservationConfig(AppConfig):
     name = 'apps.reservation'
+    verbose_name = 'Бронирование переговорных'

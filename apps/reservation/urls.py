629428b78a591cdb25b1ae36841af8772ff8820d
Ivan.Kataev <Ivan.Kataev@x5.ru>
2020-05-08 10:22:41 +0300

    Реализована возможность отбора бронирований по временному интервалу Сделал фильтр. Прописал роутер Учел, что в GET могут передавать некорректные даты Запрос типа http://127.0.0.1:8000/api/reservation/list/?start_date_range=2020-05-01%2000:00&end_date_range=2020-05-03%2000:00

--- apps/reservation/urls.py
+++ apps/reservation/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.conf.urls import url, include
 
+from rest_framework.routers import DefaultRouter
 from apps.reservation.api.views import (
     ListReservationAPIView,
     ReservationAPIView,
@@ -10,8 +12,12 @@ ListReservationRoomTodayAPIView
 
 app_name = "reservation"
 
+router = DefaultRouter(trailing_slash=False)
+router.register(r'', ListReservationAPIView)
+
 urlpatterns = [
-    path("list/", ListReservationAPIView.as_view(), name='reservation-list'),
+    # path("list/", ListReservationAPIView.as_view(), name='reservation-list'),
+    url(r"list/", include(router.urls)),
     path("list/today/", ListReservationTodayAPIView.as_view(), name='reservation-today-list'),
     path("list/today/<slug:pk>", ListReservationRoomTodayAPIView.as_view(), name='reservation-room-today-list'),
     path("<slug:pk>", ReservationAPIView.as_view(), name='reservation-detail'),
